1fa6181e1feab0a9f217e51d3d109c75ff5615ec
tr00per92 <zvezdo@mail.bg>
2014-08-26 17:33:41 +0300

    comments adding is done

--- viewPhoto.php
+++ viewPhoto.php
@@ -7,6 +7,35 @@ if (isset($_GET['img'])) { $img = $_GET['img']; ?>
     <div class="pic">
         <img src="<?=$img?>" alt="pic" style="width: 500px"/>
     </div>
+    <div class="new-comment">
+        <form method="POST">
+            <input type="text" name="author" id="author" title = "Input comment author's nickname here." placeholder="Comment author..."/><br>
+            <textarea name="comment" id="commentbox" title = "Input comment here." placeholder="Insert comment..."></textarea><br>
+            <input type="submit" value="Post Comment"/><br>
+        </form>
+        <?php
+        if(isset($_POST['author']) && isset($_POST['comment'])) {
+
+            $imgName = basename($img);
+            $comment_auth = trim($_POST['author']);
+            $comment_text = trim($_POST['comment']);
+            global $db_dsn, $db_username, $db_password;
+
+            $dbh = new PDO($db_dsn, $db_username, $db_password);
+            $sql = "SELECT photo_id FROM photos "
+                 . "WHERE photo_name = '$imgName'";
+            $q = $dbh->prepare($sql);
+            $q->execute();
+            $photo_id = $q->fetch()[0];
+
+            $sql = "INSERT INTO image_comments (comment_auth, comment_text, image_ID) "
+                 . "VALUES ('$comment_auth', '$comment_text', $photo_id)";
+            $q = $dbh->prepare($sql);
+            $q->execute();
+            echo("Comment posted.");
+        }
+        ?>
+    </div>
     <div class="comments">
 
     </div>

92670522724d4292035be2f2612d2816e3631e75
Krassimir <xhism.kml@gmail.com>
2014-08-26 16:47:31 +0300

    Split displayComments.php
    new sub-files are displayAlbumComments.php and displayImageComments.php

--- displayImageComments.php
+++ displayImageComments.php
@@ -4,14 +4,13 @@ $hostname = 'pdb11.awardspace.net';
 $dbName = '1279150_redrover';
 $db_username = '1279150_redrover';
 $db_password = 'kal!nk3moQ';
-$con = mysqli_connect($server, $user, $pass, $dbName) or die("Can't connect");
+$con = mysqli_connect($hostname, $db_username, $db_password, $dbName) or die("Can't connect");
 mysqli_set_charset($con, 'utf8');
 
-$url = getCurrentPageURL();
 $q = mysqli_query($con, 'SELECT comment_auth, comment_text, time_added'
-                        .'FROM comments '
+                        .'FROM album_comments '
                         .'ORDER BY time_added ASC '
-                        .'WHERE page_URL = $url');
+                        .'WHERE album_ID = (SELECT photo_id FROM photos');
 
 echo '<div class="comments-wrap">';
 while($row=$q->fetch_assoc()){

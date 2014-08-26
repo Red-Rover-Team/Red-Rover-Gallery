36f51c176a598bb114a28992cb8b5eaf4281174a
Mina <mina.dodunekova@gmail.com>
2014-08-26 13:25:50 +0300

    Added sample top rated html on index.php

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php require_once('includes/header.php'); ?>
 <div class="panel">
         <section>
-            <h3>Create new album</h3>
+            <header>
+                <h2>Create new album</h2>
+            </header>
             <form method="post">
                 <div class="row">
-                    <!--label for="name">Album Name: </label-->
                     <div class="4u">
+                        <label for="name">Album Name: </label>
+                    </div>
+                    <div class="8u">
                         <input type="text" id="name" name="album[]" maxlength="25" pattern=".{3,}" required
                            title="The name can contain only english characters and numbers from zero to nine." placeholder="Album Name:"/>
                     </div>
-                <!--/div-->
+                </div>
                     <!--br-->
-                    <!--label for="category">Category: </label-->
-                <!--div class="row"-->
+                    
+                <div class="row">
+                    <div class="4u">
+                        <label for="category">Category: </label>
+                    </div>
                     <div class="4u">
                         <select id="category" name="album[]" required>
                             <option hidden selected></option>
@@ -39,8 +46,26 @@
 
 
         <section>
-            <h3>Top Rated Albums</h3>
-            <p><strong>TO DO</strong></p>
+            <header>
+                <h2>TO DO - Top Rated Albums</h2>
+            </header>
+            <div class="row">
+                <div class="4u">
+                    <h3>Top Rated Album Name #1</h3>
+                    <a class="image fit" href="#"><img alt="" src="img/pic01.jpg"></a>
+                    <p>Votes: 125</p>
+                </div>
+                <div class="4u">
+                    <h3>Top Rated Album Name #2</h3>
+                    <a class="image fit" href="#"><img alt="" src="img/pic02.jpg"></a>
+                    <p>Votes: 85</p>
+                </div>
+                <div class="4u">
+                    <h3>Top Rated Album Name #3</h3>
+                    <a class="image fit" href="#"><img alt="" src="img/pic03.jpg"></a>
+                    <p>Votes: 65</p>
+                </div>
+            </div>
         </section>
 
     </div>
